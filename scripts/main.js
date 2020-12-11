fbf9c8598eac6f90c2b43bef54a5d33b9a265023
diana-wi <75399389+diana-wi@users.noreply.github.com>
2020-12-11 01:10:45 -0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -11,17 +11,21 @@ reload.onclick = function() {
     window.location.reload();
 };
 
-const updatedDate = 'December 10, 2020';
-const updatedMessage = document.querySelector('.updated-date');
-updatedMessage.textContent = `Last updated on ${updatedDate}`;
 
-// Listing of project titles
+// -------------------------------------------------
 
 const allprojects = [
     'Number Guessing Game',
     'Christmas Message Filtering',
     'Interactive Image Gallery'
 ];
+const updatedDate = 'December 10, 2020';
+const updatedMessage = document.querySelector('.updated-date');
+
+updatedMessage.textContent = `Last updated on ${updatedDate}`;
+
+
+// Listing of project titles
 
 const totalProject = allprojects.length;
 const quicklinks = document.querySelector('.quicklinks');
@@ -30,49 +34,77 @@ for (let i = 0; i < totalProject; i++) {
     const projectName = allprojects[i];
     const newProjectItem = document.createElement('li');
     const newProjectLink = document.createElement('a');
-    newProjectLink.setAttribute('href', `#${i+1}`);
     const newProjectNumber = document.createElement('span');
+    const projectTitles = document.querySelectorAll('.cardtitle');
+    
+    newProjectLink.setAttribute('href', `#${i+1}`);
     newProjectNumber.textContent = `${i+1}`;
     newProjectLink.append(newProjectNumber, projectName);
     newProjectItem.appendChild(newProjectLink);
     quicklinks.appendChild(newProjectItem);
-
-    const projectTitles = document.querySelectorAll('.cardtitle'); //select all card titles
+    
     projectTitles[i].textContent = `Project ${i+1} - ${projectName}`;
+}
+
+function random(number) {
+    let randomNumber = Math.floor(Math.random() * number) + 1;
+    return randomNumber;
 }
 
 
 // Project 1 - Number Guessing Game
 
-let randomNumber = Math.floor(Math.random() * 100) + 1; 
-
-const guess_submit = document.getElementById('guess-submit');
-const guess_input = document.getElementById('guess-input');
-const guess_result = document.getElementById('guess-result');
-const guess_history = document.getElementById('guess-history');
-const guess_form = document.querySelector('.guessing-game-form');
-const guess_feedback = document.querySelector('.guess-feedback');
 const play_guessgame = document.getElementById('play-guessgame');
+const guess_form = document.querySelector('.guessing-game-form');
+const guess_input = document.getElementById('guess-input');
+const guess_submit = document.getElementById('guess-submit');
+const guess_result = document.getElementById('guess-result');
+const guess_feedback = document.querySelector('.guess-feedback');
+const small_note = document.querySelector('.small-note');
+const guess_history = document.getElementById('guess-history');
 
-guess_history.style.display = 'none';
-
-guess_feedback.textContent = 'Computer picked a random number. Can you guess it within 10 turns?';
-let small_note = document.createElement('p'); 
-guess_result.appendChild(guess_feedback);
-guess_result.appendChild(small_note);
-
-guess_form.style.display = 'none';
-
+let randomNumber;
 let guess_count = 1;
 let guess_maxCount = 10;
-let guess_replaybutton;
 
-play_guessgame.onclick = function() {
+guess_result.append(guess_feedback, small_note);
+play_guessgame.addEventListener('click', gameStart);
+
+guessingGame();
+
+function guessingGame(option) {
+    switch (option) {
+        case 'restart':
+            play_guessgame.style.display = 'block'; 
+            play_guessgame.textContent = 'Play Again';
+            guess_form.style.display = 'none';
+            while(guess_history.hasChildNodes()) { //loops through the all children of guess history
+                guess_history.removeChild(guess_history.firstChild); //removes first child
+            }
+            break;
+        default:
+            guess_history.style.display = 'none';
+            guess_form.style.display = 'none';
+            guess_feedback.textContent = 'Computer picked a random number. Can you guess it within 10 turns?';
+            break;
+    }
+};
+
+function gameStart() {
+    randomNumber = random(100);
+    guess_count = 1;
     play_guessgame.style.display = 'none';
+    guess_form.style.display = 'block';
     guess_feedback.textContent = "Guess between 1 and 100.";
     small_note.textContent = "We'll let you know if it is low or high.";
-    guess_form.style.display = 'block';
-    guess_input.focus();
+    guess_submit.addEventListener('click', checkMyNumber);
+    guess_input.addEventListener('keyup', function(event) {
+        if (event.code === 'Enter') {
+            event.preventDefault();
+            guess_submit.click();
+        }
+    });
+    clearInput();
 };
 
 function checkMyNumber() {
@@ -98,7 +130,7 @@ function checkMyNumber() {
             clearInput();
         } else if (myNumber < 0) {
             guess_feedback.textContent = "No negative number.";
-            small_note.textContent = 'We only accept positive number... for positive vibe.';
+            small_note.textContent = 'Positive number... for positive vibe.';
             clearInput();       
         } else {
             guess_history.style.display = 'block';
@@ -106,83 +138,34 @@ function checkMyNumber() {
             if (myNumber === randomNumber) {
                 guess_feedback.textContent = `${myNumber} is correct!`;
                 small_note.textContent = "That was fun! Let's play again.";
-                guess_history.style.display = 'none';
-                play_again();     
+                guessingGame('restart'); 
             } else if (guess_count === guess_maxCount) {
                 guess_feedback.textContent = 'GAME OVER';
                 small_note.textContent = "You ran out of turns. Play again?";
-                guess_history.style.display = 'none';
-                play_again();
+                guessingGame('restart');   
             } else {
+                const myPastGuessing = document.createElement('li');
+                let lowOrHigh;
                 small_note.textContent = '';
-                showGuessNumber();
+                if (myNumber < randomNumber) {
+                    lowOrHigh = 'Low';
+                    guess_feedback.textContent =  `${myNumber} is ${lowOrHigh}`;
+                } else if (myNumber > randomNumber) {
+                    lowOrHigh = 'High';
+                    guess_feedback.textContent = `${myNumber} is ${lowOrHigh}`;
+                }
+
+                if (myNumber > 100) {
+                    guess_feedback.textContent = `${myNumber}?! Try guessing a number between 1 and 100.`;
+                } 
+                myPastGuessing.textContent = `${myNumber} ${lowOrHigh}`;
+                guess_history.insertBefore(myPastGuessing, guess_history.firstChild); //insert the newly created list to the top.
+                
                 guess_count++;
                 clearInput();
             } 
         }  
     }     
-};
-
-guess_submit.onclick = function() {
-    checkMyNumber();
-};
-
-guess_input.addEventListener("keyup", function(event) {
-    if (event.code === 'Enter') {
-        event.preventDefault();
-        guess_submit.click();
-    }
-});
-
-function showGuessNumber() {
-    let myNumber = Number(guess_input.value);
-    const guess_item = document.createElement('li');
-    let guess_hint;
-
-    if (myNumber < randomNumber) {
-        guess_hint = 'Low';
-        guess_feedback.textContent =  `${myNumber} is ${guess_hint}`;
-    } else if (myNumber > randomNumber) {
-        guess_hint = 'High';
-        guess_feedback.textContent = `${myNumber} is ${guess_hint}`;
-    }
-
-    if (myNumber > 100) {
-        guess_feedback.textContent = `${myNumber}?! Try guessing a number between 1 and 100.`;
-    } 
-
-    guess_item.textContent = `${myNumber} ${guess_hint}`;
-    guess_history.insertBefore(guess_item, guess_history.firstChild); //insert the newly created list to the top.
-}
-
-function play_again() {
-    guess_input.style.display = 'none';
-    guess_submit.style.display = 'none';
-
-    guess_replaybutton = document.createElement('button'); //create reset button
-
-    guess_replaybutton.textContent = 'Play Again';
-    guess_form.insertBefore(guess_replaybutton, guess_form.firstChild);
-    guess_replaybutton.addEventListener('click', resetGuessGame);
-
-    while(guess_history.hasChildNodes()) { //loops through the all children of guess history
-        guess_history.removeChild(guess_history.firstChild); //removes first child
-    }
-};
-
-function resetGuessGame() {
-    randomNumber = Math.floor(Math.random() * 100) + 1; 
-    guess_count = 1;
-    guess_history.style.display = 'none';
-    guess_form.removeChild(guess_form.firstChild);
-
-    guess_input.style.display = 'initial';
-    guess_submit.style.display = 'initial';
-    clearInput();
-
-    guess_submit.textContent = 'Guess';
-    guess_feedback.textContent = 'Computer picked a new random number.';
-    small_note.textContent = 'Guess the correct numb    er within 10 turns.';   
 };
 
 function clearInput() {
@@ -242,7 +225,7 @@ christmasFilter.onclick = function() {
 
 
 
-// Project 3 - Image Gallery
+// Project 3 - Interactive Image Gallery
 
 const fullimage = document.querySelector('.fullimage');
 const thumbnails = document.querySelector('.thumbnails');
